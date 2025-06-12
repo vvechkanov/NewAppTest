bfb937d1ae59711b5396f97c585af9fe9d2c5e27
Vladimir Vechkanov <vechkanov90@gmail.com>
2025-06-12 20:32:32 +0200

    Fix init and start scripts

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
+cd "$(dirname "$0")/.."
 pnpm install
 pnpm approve-builds
 PRISMA_ENGINES_CHECKSUM_IGNORE_MISSING=1 pnpm --filter @new-app/api exec prisma generate --skip-download || true
